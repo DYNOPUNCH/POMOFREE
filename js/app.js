0060f012125684825ef44c939ecc288bb37b3eaf
DYNO <78282488+DYNOPUNCH@users.noreply.github.com>
2024-11-16 17:54:30 -0500

    added picture of nano and debugged the pomo form popup

--- js/app.js
+++ js/app.js
@@ -1,12 +1,25 @@
 
+const addTaskFormBlock = $('#task-form-template').html();
+const addTaskBlock = $('#task-template').html();
 
-$('#add-new-task-button').on('click', function () { $('#task-list').append('<div class=\"container d-flex justify-content-center bg-primary py-3 mb-3 fw-bold text-light fs-5 rounded \">New Task</div>') });
+$("#timer").timer({
+    countdown: true,
+    duration: '25m'
+}).timer('pause');
+
+
+$('#add-new-task-button').on('click', function () {
+    $('#overlay-holder').append(addTaskFormBlock);
+    $('#form-close').on('click', function () {$('#overlay-holder').empty();})
+    $('#add-task').on('click', function () {
+        $('#task-list').append(addTaskBlock);
+        $('#overlay-holder').empty();
+    })
+}
+);
 $('#action-button').on('click', function () {
-    $('#timer').timer({
-        countdown: true,
-        duration: '25m',    	// This will start the countdown from 3 mins 40 seconds
-        callback: function() {	// This will execute after the duration has elapsed
-            console.log('Time up!');
-        }
-    });
+    $('#timer').timer('resume');
 })
+
+
+//$('#task-list').append('<div class=\"container d-flex justify-content-center bg-primary py-3 mb-3 fw-bold text-light fs-5 rounded \">New Task</div>')
